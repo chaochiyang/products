9b31dcc1159fa8373e100a936d7be24b72f23942
Simon <chaochiyang@gmail.com>
2021-09-13 16:15:02 +0800

    add file exist checking

--- products_2.py
+++ products_2.py
@@ -1,19 +1,19 @@
 import os #import 作業系統來溝通
 
+products = []
 if os.path.isfile('products.csv'):    # 請問OS有沒有這個檔案or地址
-	print('yes! 找到檔案')
+	print('yes! 找到檔案了！')
+	with open('products.csv','r',encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:      
+				continue 				# header 不儲存，直接跳過這一回合
+			name, price = line.strip().split(',')   # 先用strip去掉換行符號，再split用,當作切割的標準
+			products.append([name, price])
+	print(products)
+
 else:
 	print('找不到檔案....')
 
-#讀取檔案
-products = []
-with open('products.csv','r',encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:      
-			continue 				# header 不儲存，直接跳過這一回合
-		name, price = line.strip().split(',')   # 先用strip去掉換行符號，再split用,當作切割的標準
-		products.append([name, price])
-print(products)
 
 # 讓使用者輸入
 while True:
@@ -29,7 +29,7 @@ for p in products:
 	print(p[0],'的價格:',p[1])
 
 # 寫入檔案
-with open('products.csv','w',encoding = 'utf-8') as f :
+with open('products.csv','w',encoding = 'utf-8') as f:
 	f.write('商品,價格\n') #寫入欄位名稱
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
